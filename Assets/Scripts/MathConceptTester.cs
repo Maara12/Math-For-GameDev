a6d36dba83923933e78169129f1754060abba2ab
Maara <92599683+Maara12@users.noreply.github.com>
2025-02-27 11:36:27 +0530

    C2 round, floor, ceil test

--- Assets/Scripts/MathConceptTester.cs
+++ Assets/Scripts/MathConceptTester.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class MathConceptTester : MonoBehaviour
@@ -7,11 +9,21 @@ public class MathConceptTester : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
-            CheckINtValueOverFlowInConsole();
+            RoundNumber();
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            CeilNumber();
+        }
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            FloorNumber();
         }
     }
 
-    //CHAPTER 1 ADDITION AND SUBTRACTION
+    #region CHAPTER 1 ADDITION AND SUBTRACTION
+        
+    
       
     public int a = 2147483640;
 
@@ -20,4 +32,30 @@ public class MathConceptTester : MonoBehaviour
         a = a + 1;
         Debug.Log(a);
     }
+
+    #endregion 
+
+    #region CHAPTER 2 ROUNDING, CEILING AND FLOOR
+        
+    public float numberToApproximate = 6.283f;
+
+    public void RoundNumber()
+    {
+        Debug.Log("Rounded number: " + Math.Round(numberToApproximate,2)); // general use
+        Debug.Log("Rounded number: " + Mathf.Round(numberToApproximate * 100) / 100); // used for and floats, good for performance
+    }
+
+    public void FloorNumber()
+    {
+        Debug.Log("Floored number: " + Mathf.Floor(numberToApproximate));
+    }
+
+    public void CeilNumber()
+    {
+        Debug.Log("Ceiled number: " + Mathf.Ceil(numberToApproximate));
+    }
+
+    #endregion
+    
+    
 }
